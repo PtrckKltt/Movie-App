3f7df38ae187c4151f9cc0357added18020ab50b
Patrick Klatt <klatt@bau-abc-rostrup.de>
2026-02-15 16:24:54 +0100

    Feature: implemented navigation Bar functions

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,16 @@
 import { Stack } from "expo-router";
 import './globals.css';
+import {App} from "expo-router/build/rsc/entry";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+    <Stack.Screen
+        name="(tabs)"
+        options={{ headerShown: false }}
+    />
+    <Stack.Screen
+        name="movie/[id]"
+        options={{ headerShown: false }}
+    />
+  </Stack>
 }
